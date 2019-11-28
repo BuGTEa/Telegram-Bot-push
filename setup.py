45defa14f0fad032175528a93e9e698d2ccc9ee6
BuGTEa <bpan160901@gmail.com>
2019-11-28 15:24:19 +0800

    Add Pypi Description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
 	name='tele-bot-push',
-	version='0.0.2',
+	version='0.0.2.1',
 	author='BuGTEa',
 	author_email='noreply@sir7878.com',
 	url='https://github.com/BuGTEa/Telegram-Bot-push',
